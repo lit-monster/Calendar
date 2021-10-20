82b5142472a3a14ee38c40dabc2d3d800bee147c
AobaSuzuki <aoba.suzuki7321@gmail.com>
2021-10-20 20:37:27 +0900

    rip cocoa pods

--- Calendar/ChartViewController.swift
+++ Calendar/ChartViewController.swift
@@ -6,99 +6,10 @@
 //
 
 import UIKit
-import Charts
 
-class ChartViewController: UIViewController,ChartViewDelegate {
-    
-    var barChart = BarChartView()
+class ChartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        barChart.delegate = self
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        barChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height:              self.view.frame.size.width)
-        
-        barChart.center = view.center
-        view.addSubview(barChart)
-        
-        
-        var entries = [BarChartDataEntry]()
-        
-        for x in 0..<7{
-            entries.append(BarChartDataEntry(x: Double(x), y: Double(x)))
-        }
-        
-        let set = BarChartDataSet(entries: entries)
-        set.colors = ChartColorTemplates.joyful()
-        let data = BarChartData(dataSet: set)
-        barChart.data = data
-    }
-
-
 }
-
-
-//class ChartViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
-//
-
-
-
-
-//class ChartViewController: UIViewController {
-//
-//
-//    let records: [CGFloat] = [0.2, 0.5, 0.4, 0.6, 0.2, 0.9, 1.0, 0.4]
-//
-//    @IBOutlet var tableView: UITableView! {
-//        didSet {
-//            tableView.delegate = self
-//            tableView.dataSource = self
-//            self.tableView?.register(UINib(nibName: "GraphTableViewCell", bundle: nil), forCellReuseIdentifier: "GraphTableViewCell")
-//        }
-//    }
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//    }
-//}
-//
-//extension ChartViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return records.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GraphTableViewCell", for: indexPath) as? GraphTableViewCell else { fatalError() }
-//        cell.width = records[indexPath.row]
-//        return cell
-//
-//
-//    }
-//
-//
-//}
-//
