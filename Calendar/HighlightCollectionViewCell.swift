0ab64b524cf3900b9c0a358b7bc8c4dedef08f99
AobaSuzuki <aoba.suzuki7321@gmail.com>
2022-04-04 17:00:50 +0900

    1週間分のデータ

---
+++ Calendar/HighlightCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  HighlightCollectionViewCell.swift
+//  Calendar
+//
+//  Created by 鈴木　葵葉 on 2022/04/04.
+//
+
+import UIKit
+
+class HighlightCollectionViewCell: UICollectionViewCell {
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+//        self.view.innerShadow()
+    }
+}
+
+
+extension UIView {
+    func innerShadow() {
+        let path = UIBezierPath(rect: CGRect(x: -5.0, y: -5.0, width: self.bounds.size.width + 5.0, height: 5.0 ))
+        let innerLayer = CALayer()
+        innerLayer.frame = self.bounds
+        innerLayer.masksToBounds = true
+        innerLayer.shadowColor = UIColor.black.cgColor
+        innerLayer.shadowOffset = CGSize(width: 2.5, height: 2.5)
+        innerLayer.shadowOpacity = 0.5
+        innerLayer.shadowPath = path.cgPath
+        self.layer.addSublayer(innerLayer)
+    }
+}
+
