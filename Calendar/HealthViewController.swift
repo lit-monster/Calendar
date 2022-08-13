c34fb69d907eee9ba5f2f135379663fe92bdc608
AobaSuzuki <aoba.suzuki7321@gmail.com>
2022-08-13 14:19:18 +0900

    a

--- Calendar/HealthViewController.swift
+++ Calendar/HealthViewController.swift
@@ -10,14 +10,39 @@ import UIKit
 import HealthKit
 
 class HaelthViewController: UIViewController {
+    let myHealthStore = HKHealthStore()
+    var typeOfHeartRate = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
     
     @IBOutlet weak var HeartRateView: UIView!
    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //healthkit使用の許可
+        let typeOfRead = Set([typeOfHeartRate])
+        myHealthStore.requestAuthorization(toShare: [],read: typeOfRead,completion: { (success, error) in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            print(success)
+        })
+        
+        readHeartRate()
         
     }
-    
+    func readHeartRate() {
+        //期間の設定
+        let calendar = Calendar.current
+        let date = Date()
+        let endDate = calendar.date(byAdding: .day, value: -0, to: calendar.startOfDay(for: date))
+        let startDate = calendar.date(byAdding: .day, value: -1, to: calendar.startOfDay(for: date))
+        
+        let query = HKSampleQuery(sampleType: HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!, predicate: HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: []), limit: HKObjectQueryNoLimit, sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)]){ (query, results, error) in
+            //ここに処理書く
+            print(results!)//←心拍数がprintされるはず
+        }
+        myHealthStore.execute(query)
+    }
    
 }
