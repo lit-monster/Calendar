b46bd06c08f133678433c5bd40d32fd8b4d84616
AobaSuzuki <aoba.suzuki7321@gmail.com>
2022-08-13 12:31:54 +0900

    HealthKit

---
+++ Calendar/HealthViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HealthViewController.swift
+//  Calendar
+//
+//  Created by 鈴木　葵葉 on 2022/08/13.
+//
+
+import Foundation
+import UIKit
+import HealthKit
+
+class HaelthViewController: UIViewController {
+    
+    @IBOutlet weak var HeartRateView: UIView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+   
+}
