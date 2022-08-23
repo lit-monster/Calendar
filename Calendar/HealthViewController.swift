18613ca12b5b46e4ac73bc2755941b16c1094348
AobaSuzuki <aoba.suzuki7321@gmail.com>
2022-08-23 17:00:55 +0900

    心拍取り出せた

--- Calendar/HealthViewController.swift
+++ Calendar/HealthViewController.swift
@@ -13,7 +13,10 @@ class HaelthViewController: UIViewController {
     let myHealthStore = HKHealthStore()
     var typeOfHeartRate = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!
     
-    @IBOutlet weak var HeartRateView: UIView!
+//    @IBOutlet weak var HeartRateView: UIView!
+    
+    var heartRateArray: [Double] = []
+    
    
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,13 +39,25 @@ class HaelthViewController: UIViewController {
         let calendar = Calendar.current
         let date = Date()
         let endDate = calendar.date(byAdding: .day, value: -0, to: calendar.startOfDay(for: date))
-        let startDate = calendar.date(byAdding: .day, value: -1, to: calendar.startOfDay(for: date))
+        let startDate = calendar.date(byAdding: .day, value: -10, to: calendar.startOfDay(for: date))
+        
+        let heartRateUnit:HKUnit = HKUnit(from: "count/min")
         
         let query = HKSampleQuery(sampleType: HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!, predicate: HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: []), limit: HKObjectQueryNoLimit, sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)]){ (query, results, error) in
-            //ここに処理書く
-            print(results!)//←心拍数がprintされるはず
+            
+            print(results![0])
+            
+            for result in results ?? [] {
+                guard let currData = result as? HKQuantitySample else { return }
+                print("心拍数: \(currData.quantity.doubleValue(for: heartRateUnit))")
+                let heartRate = currData.quantity.doubleValue(for: heartRateUnit)
+                self.heartRateArray.append(heartRate)
+            }
+            
         }
         myHealthStore.execute(query)
+        print("心拍数の配列")
+        print(heartRateArray)
     }
    
 }
