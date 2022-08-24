9e230ce6d3932a29367f4a788439855793077b94
AobaSuzuki <aoba.suzuki7321@gmail.com>
2022-08-24 12:11:46 +0900

    print

--- Calendar/HealthViewController.swift
+++ Calendar/HealthViewController.swift
@@ -39,14 +39,20 @@ class HaelthViewController: UIViewController {
         let calendar = Calendar.current
         let date = Date()
         let endDate = calendar.date(byAdding: .day, value: -0, to: calendar.startOfDay(for: date))
-        let startDate = calendar.date(byAdding: .day, value: -3, to: calendar.startOfDay(for: date))
+        let startDate = calendar.date(byAdding: .day, value: -0, to: calendar.startOfDay(for: date))
+        print("startDate")
+        print(startDate)
+        print("endDate")
+        print(endDate)
         
         let heartRateUnit:HKUnit = HKUnit(from: "count/min")
         
+        //resultsに指定した期間のヘルスデータが取得される
         let query = HKSampleQuery(sampleType: HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)!, predicate: HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: []), limit: HKObjectQueryNoLimit, sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)]){ [self] (query, results, error) in
             
             print(results![0])
             
+            //heartRateArrayに心拍数の配列を代入
             for result in results ?? [] {
                 guard let currData = result as? HKQuantitySample else { return }
                 //                print("心拍数: \(currData.quantity.doubleValue(for: heartRateUnit))")
@@ -54,21 +60,22 @@ class HaelthViewController: UIViewController {
                 self.heartRateArray.append(heartRate)
             }
             print(self.heartRateArray)
-            let heart = heartRateArray
             
+            // aveHeartRateに平均心拍数を代入
+            let heart = heartRateArray
+            guard heart == [] else { return }
             let sum = self.heartRateArray.reduce(0) {(num1: Double, num2: Double) -> Double in
                 return num1 + num2
             }
             print(sum/Double(heart.count))
+            let aveHeartRate = sum/Double(heart.count)
             
-            //numにリアルタイムか直近の心拍数を入れたい
-            let num = sum/Double(heart.count)
-             
-            if ( num > sum/Double(heart.count) + 1 ) {
+             // 平均心拍数からスコアを判定
+            if ( aveHeartRate > sum/Double(heart.count) + 1 ) {
                 print("超集中")
-            } else if ( sum/Double(heart.count) + 1 > num &&  num > sum/Double(heart.count) - 1 ) {
+            } else if ( sum/Double(heart.count) + 1 > aveHeartRate &&  aveHeartRate > sum/Double(heart.count) - 1 ) {
                 print("集中")
-            } else if ( sum/Double(heart.count) - 1 > num) {
+            } else if ( sum/Double(heart.count) - 1 > aveHeartRate) {
                 print("普通")
             } else {
                 print("error")
