507ed1c770ad37fdf3045f76ce71dd63d516a955
AobaSuzuki <aoba.suzuki7321@gmail.com>
2023-01-13 18:45:16 +0900

    当たり判定怪しめなコロコロUI

---
+++ Watch Calendar Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Watch Calendar Watch App
+//
+//  Created by 鈴木　葵葉 on 2023/01/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Watch Calendar Watch App/Watch_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_CalendarApp.swift
+//  Watch Calendar Watch App
+//
+//  Created by 鈴木　葵葉 on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Calendar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
